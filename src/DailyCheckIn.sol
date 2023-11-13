4f324ca79ac307fc8832d30a2b37ae5fc55fd7f2
qpdk777 <1981315951@qq.com>
2023-11-13 16:28:02 +0800

    add openzeppelin contracts to lib

--- src/DailyCheckIn.sol
+++ src/DailyCheckIn.sol
@@ -1,7 +1,30 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.18;
 
-contract DailyCheckIn {
-    constructor() {}
+contract DailyCheckIn is Ownable {
+    constructor() Ownable(msg.sender) {}
 }

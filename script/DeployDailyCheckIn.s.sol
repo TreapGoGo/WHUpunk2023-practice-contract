cb044ee7b510d0b5b9002e43808ccf35c38b9506
qpdk777 <1981315951@qq.com>
2023-11-13 20:55:18 +0800

    update src

---
+++ script/DeployDailyCheckIn.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DailyCheckIn} from "../src/DailyCheckIn.sol";
+
+contract DeployDailyCheckIn is Script {
+    constructor() {}
+
+    function run() external {}
+}
--- src/DailyCheckIn.sol
+++ src/DailyCheckIn.sol
@@ -26,5 +26,35 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.18;
 
 contract DailyCheckIn is Ownable {
-    constructor() Ownable(msg.sender) {}
+    /* Errors */
+    error DailyCheckIn__NotEnoughTimeHasPassed(uint256 timeUntilNextCheckIn);
+
+    /* State Variables */
+    uint256 public immutable checkInTimeInterval;
+    mapping(address => uint256) addressToCheckInTimes;
+    mapping(address => uint256) addressToLastCheckInTimestamp;
+
+    /* Events */
+    event checkInSuccess(address indexed userAddress, uint256 indexed checkInTimestamp, uint256 nowCheckInTimes);
+
+    /* Modifiers */
+    modifier enoughTimeHasPassed() {
+        uint256 lastCheckInTimestamp = addressToLastCheckInTimestamp[msg.sender];
+        if (block.timestamp - lastCheckInTimestamp < checkInTimeInterval) {
+            revert DailyCheckIn__NotEnoughTimeHasPassed(lastCheckInTimestamp + checkInTimeInterval - block.timestamp);
+        }
+        _;
+    }
+
+    /* Functions */
+    constructor(uint256 _chekInTimeInterval) Ownable(msg.sender) {
+        checkInTimeInterval = _chekInTimeInterval;
+    }
+
+    function checkIn() public enoughTimeHasPassed {
+        addressToCheckInTimes[msg.sender]++;
+        addressToLastCheckInTimestamp[msg.sender] = block.timestamp;
+
+        emit checkInSuccess(msg.sender, block.timestamp, addressToCheckInTimes[msg.sender]);
+    }
 }
